84957f03962db87f1dbb47691e413ed73487b1d1
ckwang19 <ckwang19@gmail.com>
2020-04-05 15:06:11 +0800

    first commit

--- Main.py
+++ Main.py
@@ -4,9 +4,10 @@ sys.path.insert(-1, '/usr/local/lib/python2.7/dist-packages')
 import argparse
 
 from module.ParserYahooFin.get_yahoo_fin import download_quotes as parser_day_info
-from module.ParserStatement.get_statement import Parser
+from module.ParserTWSE.get_twse import Parser
 from module.XMLWriter.XMLGenerator import xml_writer
 #import initial
+import time
 
 
 
@@ -34,35 +35,31 @@ def main():
     with open('stock_num.txt', 'r') as f_r:
         for line in f_r.readlines():
             stock_num_list.append(line.strip()[0:4])
-
-    for stock_num in stock_num_list:
-        if stock_num in finish_stock_num_list:
-            continue
+    if True:
+        stock_num = 2002
+#    for stock_num in stock_num_list:
+        #if stock_num in finish_stock_num_list:
+        #    continue
         print ('right now is {}, still have {} stock unfinish'.format(stock_num, len(stock_num_list) - count))
-        try:
-            obj.parser_eps(stock_num)
-            obj.parser_book_value(stock_num)
-            obj.parser_cash_flow(stock_num)
-            obj.parser_profit_margin(stock_num)
-            obj.parser_non_oper_income(stock_num)
-            obj.parser_roe(stock_num)
-            obj.parser_oper_cash_flow_income_rate(stock_num)
-            obj.parser_turnover_days(stock_num)
 
-            obj.parser_price_earnings_ratio(stock_num)
-            obj.parser_mth_revenue_growth_rate(stock_num)
-            obj.parser_average_dividend_yield(stock_num)
+        for typ in []:
+            for y in range(95, 109+1):
+                for m in range(1, 12+1):
+                    for d in range(1, 31+1):
+                        try:
+                            #obj.parser_BWIBBU_d(stock_num, y, m, d)
+                            obj.parser_general_TWSE(typ, y, m, d)
+                        except:
+                            print (y, m, d)
+                        time.sleep(3)
+        #    try:
+        #        parser_day_info('{}.TW'.format(stock_num))
+        #    except:
+        #        parser_day_info('{}.TWO'.format(stock_num))
+        #    xml_writer(obj.dict_all, obj.xml_bs_pth, stock_num)
+        #except:
+        #    print ('{} has something wrong'.format(stock_num))
 
-            obj.parser_stock_holders(stock_num)
-            try:
-                parser_day_info('{}.TW'.format(stock_num))
-            except:
-                parser_day_info('{}.TWO'.format(stock_num))
-            xml_writer(obj.dict_all, obj.xml_bs_pth, stock_num)
-        except:
-            print ('{} has something wrong'.format(stock_num))
-        obj.clear()
-        count += 1
 
 #def main():
 #	param = get_args()
